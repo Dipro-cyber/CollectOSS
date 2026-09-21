13fd785140f0b07a89987ecfdd7cee9b2667923c
Adrian Edwards <adredwar@redhat.com>
2026-09-21 19:46:13 -0400

    disable heavy mathjax extension
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     # 'sphinx.ext.githubpages',
