294e61f0b91ed6f060d0b74035b48b9ca06556ec
Adrian Edwards <adredwar@redhat.com>
2026-09-21 19:29:43 -0400

    dont include todos in prod docs builds
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -110,7 +110,7 @@ exclude_patterns = []
 pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
+todo_include_todos = False # enable this in dev if desired
 
 html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'] }
 
